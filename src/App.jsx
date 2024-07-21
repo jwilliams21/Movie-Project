fbdd1bb7345b42b7980b15ccc88c565bdece404e
Josh Williams <jwilliams@standpointtech.com>
2024-07-21 14:06:12 -0400

    First Pass at NavBar
    First Pass at NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Modal, Button } from 'bootstrap'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button } from 'bootstrap';
+import { NavBar } from './NavBar';
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <NavBar />
   )
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Modal, Button } from 'bootstrap'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button } from 'bootstrap';
+import OscarLogo  from './assets/OscarLogo.png';
+import './NavBarStyle.css';
 
 export function NavBar () {
+
+ return ( 
     <>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="navbar">
+            <div className="navheader">
+                <img src={OscarLogo} alt="OscarLogo"/>
+            </div>
+            <div className="navheader">
+                Best Picture
+            </div>
+            <div className="navheader">
+                Best Director
+            </div>
+            <div className="navheader">
+                Best Actor
+            </div>
+            <div className="navheader">
+                Best Actress
+            </div>
+            <div className="navheader">
+                Best Supporting Actor
+            </div>
+            <div className="navheader">
+                Best Supporting Actress
+            </div>
         </div>
     </>
+ )  
 }
