2476aaf44ca7f2acf5b1e2ce162a0ff15c50dd43
Josh Williams <jwilliams@standpointtech.com>
2024-07-21 14:59:45 -0400

    Homepage Commit New Images Dummy Text
    Homepage Commit New Images Dummy Text

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,33 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button } from 'bootstrap';
 import { NavBar } from './NavBar';
+import EveryoneOscar from './assets/EveryoneOscar.jpg';
+import Homepage1 from './assets/Homepage1.webp';
+import Homepage2 from './assets/Homepage2.jpg';
+import './HomeStyle.css';
 
 export function Home() {
 
 
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <body>
+        <div>
+          <h4><b><u>The Academy</u></b></h4>
+          <br/>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+             Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <div className="homepage-image-container"> 
+            <img src={EveryoneOscar} alt="EveryoneOscar" className="EveryoneOscar"/>   
+            <img src={Homepage1} alt="Homepage1" className="Homepage1" style={{ position: 'relative', zIndex: 1 }}/>   
+            <img src={Homepage2} alt="Homepage2" className="Homepage2"/> 
+          </div>    
+        </div>
+      </body>
+    </>
   )
 }
 
