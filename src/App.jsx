74dcb40a6031b21c58f803f33a76166f7f780ab4
Josh Williams <jwilliams@standpointtech.com>
2024-07-21 13:33:12 -0400

    Initial Packages
    Initial Packages

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Modal, Button } from 'bootstrap'
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Modal, Button } from 'bootstrap'
+
+export function NavBar () {
+    <>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </>
+}
